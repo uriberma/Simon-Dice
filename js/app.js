bec47a84bb3c234f21e86bfa942a4019846f3494
Maycol Uribe Romero <maycol.uribe-romero@hotmail.com>
2019-12-28 18:32:49 +0100

    illumination of sequence by level implemented

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 /*  Variables  */
-let game = document.querySelector('.game');
 let blue = document.querySelector('.blue');
 let violet = document.querySelector('.violet');
 let orange = document.querySelector('.orange');
@@ -13,9 +12,59 @@ class Game {
 
     init() {
         startGameBtn.classList.add('hide');
+        this.generateSequence();
+        this.level = 10;
+        this.colors = {
+            blue,
+            violet,
+            orange,
+            green
+        }
+        this.nextLevel()
+    }
+
+    generateSequence() {
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
+    nextLevel() {
+        this.illuminateSequence()
+    }
+
+    transformNumberToColor(num) {
+        switch(num) {
+            case 0: 
+                return 'blue'
+            case 1: 
+                return 'violet'
+            case 2: 
+                return 'orange'
+            case 3:
+                return 'green'
+        }
+    }
+
+    illuminateSequence() {
+        for (let i=0; i<this.level; i++) {
+            const color = this.transformNumberToColor(this.sequence[i]);
+            setTimeout(() => {
+                this.illuminateColor(color);
+            }, 1000 * i)
+        }
+    }
+
+    illuminateColor(color) {
+        this.colors[color].classList.add('light');
+        setTimeout(() =>{
+            this.turnOffColorLight(color)
+        },450)
+    }
+
+    turnOffColorLight(color) {
+        this.colors[color].classList.remove('light')
     }
 }
 
 function startGame() {
-    let game = new Game();
+    window.game = new Game();
 }
